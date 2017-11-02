c54151aa6049d04a28b8c581a479cf3e63ef121b
Yamuna Navada <yamunanavada@gmail.com>
2017-11-02 16:44:35 +0000

    automatically backed up by learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -5,3 +5,15 @@ end
 def array_with_two_elements
   new_array = [1,2]
 end
+
+def first_element(array)
+  array[0]
+end
+
+def third_element(array)
+  array[2]
+end
+
+def last_element(array)
+  array[array.length-1]
+end
